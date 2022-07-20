32febffa0f285069c3952cb910dd76b82ce00737
I Putu Sura Sanjaya <putu.sanjaya@amartha.com>
2022-07-20 23:29:11 +0800

    feat: add login endpoint

--- handler/user.go
+++ handler/user.go
@@ -34,3 +34,28 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	response := helper.ApiResponse("Your account has been created", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	input := user.LoginInput{}
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errMessage := gin.H{"errors": errors}
+		response := helper.ApiResponse("Login failed", http.StatusUnprocessableEntity, "error", errMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	loggedinUser, err := h.service.Login(input)
+	if err != nil {
+		errMessage := gin.H{"errors": err.Error()}
+		response := helper.ApiResponse("Email or password is not valid", http.StatusUnprocessableEntity, "error", errMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokeninisangatrahasia")
+	successResponse := helper.ApiResponse("Login successfuly", http.StatusOK, "success", formatter)
+	c.JSON(http.StatusOK, successResponse)
+}
--- user/repository.go
+++ user/repository.go
@@ -8,21 +8,32 @@ import (
 
 type Repository interface {
 	Save(user User) (User, error)
+	FindByEmail(email string) (User, error)
 }
 
 type repository struct {
-	DB *gorm.DB
+	db *gorm.DB
 }
 
 func NewRepository(db *gorm.DB) *repository {
-	return &repository{DB: db}
+	return &repository{db}
 }
 
 func (r *repository) Save(user User) (User, error) {
-	err := r.DB.Create(&user).Error
+	err := r.db.Create(&user).Error
 	if err != nil {
 		return user, err
 	}
 	fmt.Println("Success save new user to the database")
+	return user, nil
+}
+
+func (r *repository) FindByEmail(email string) (User, error) {
+	var user User
+	err := r.db.Where("email = ?", email).Find(&user).Error
+	if err != nil {
+		return user, err
+	}
+
 	return user, nil
 }
