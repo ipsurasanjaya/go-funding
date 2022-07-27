b791340408851620cc924e2ad8fa875037a9c2c5
I Putu Sura Sanjaya <putu.sanjaya@amartha.com>
2022-07-27 11:42:45 +0800

    feat: add slug field to response struct

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,13 +1,14 @@
 package campaign
 
 type FormatCampaign struct {
-	ID            int    `json:"id"`
-	UserID        int    `json:"user_id"`
-	Name          string `json:"name"`
-	Summary       string `json:"summary"`
-	TargetAmmount int    `json:"target_amount"`
 	CurrentAmount int    `json:"current_amount"`
 	FileName      string `json:"file_name"`
+	ID            int    `json:"id"`
+	Name          string `json:"name"`
+	Summary       string `json:"summary"`
+	Slug          string `json:"slug"`
+	TargetAmmount int    `json:"target_amount"`
+	UserID        int    `json:"user_id"`
 }
 
 func CampaignsFormatter(campaigns []Campaign) []FormatCampaign {
@@ -20,13 +21,16 @@ func CampaignsFormatter(campaigns []Campaign) []FormatCampaign {
 
 func CampaignFormatter(campaign Campaign) FormatCampaign {
 	var newCampaign FormatCampaign
-	newCampaign.ID = campaign.ID
-	newCampaign.UserID = campaign.UserID
-	newCampaign.Name = campaign.Name
-	newCampaign.Summary = campaign.Summary
-	newCampaign.TargetAmmount = campaign.TargetAmount
+
 	newCampaign.CurrentAmount = campaign.CurrentAmount
 	newCampaign.FileName = ""
+	newCampaign.ID = campaign.ID
+	newCampaign.Name = campaign.Name
+	newCampaign.Summary = campaign.Summary
+	newCampaign.Slug = campaign.Slug
+	newCampaign.TargetAmmount = campaign.TargetAmount
+	newCampaign.UserID = campaign.UserID
+
 	if len(campaign.CampaignImages) != 0 {
 		newCampaign.FileName = campaign.CampaignImages[0].FileName
 	}
